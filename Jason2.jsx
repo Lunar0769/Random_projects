fe2eec66f595fc162fc0f6ce5ffe119925b8013e
Kavya Patel <pkavya085@gmail.com>
2025-08-15 21:47:41 +0530

    Delete Jason2.jsx

--- Jason2.jsx
+++
@@ -1,9 +0,0 @@
-function Jason2(JS2){
-    return(
-        <>
-            <h2>CAr Brand: {JS2.car.brand}</h2>
-            <h2>CAr Name: {JS2.car.name}</h2>
-        </>
-    )
-}
-export default Jason2
